8b23e919404566224c3a4cc76e36a493ce2360dc
13772846495 <934007690@qq.com>
2021-05-10 21:34:53 +0800

    React-Context学习

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import React from 'react';
+import React from 'react';
 import ReactDOM, { createPortal } from 'react-dom';
 import './index.css';
 
@@ -668,3 +668,142 @@ import './index.css';
 //   <MyComponent />,
 //   document.getElementById('root')
 // )
+
+
+// 不使用Context的传递
+// class App extends React.Component {
+//   render() {
+//     return <Toolbar theme="dark" />;
+//   }
+// }
+
+// function Toolbar(props) {
+//   // Toolbar 组件接受一个额外的“theme”属性，然后传递给 ThemedButton 组件。
+//   // 如果应用中每一个单独的按钮都需要知道 theme 的值，这会是件很麻烦的事，
+//   // 因为必须将这个值层层传递所有组件。
+//   return (
+//     <div>
+//       <ThemedButton theme={props.theme} />
+//     </div>
+//   );
+// }
+
+// class ThemedButton extends React.Component {
+//   render() {
+//     return <Button theme={this.props.theme} />
+//   }
+// }
+
+// function Button(props) {
+//   return (
+//     <div>
+//       {props.theme}
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// )
+
+
+// 使用Context的传递
+
+// Context 可以让我们无须明确地传遍每一个组件，就能将值深入传递进组件树。
+// 为当前的 theme 创建一个 context（“light”为默认值）。
+// const ThemeContext = React.createContext('light');
+// class App extends React.Component {
+//   render() {
+//     // 使用一个 Provider 来将当前的 theme 传递给以下的组件树。
+//     // 无论多深，任何组件都能读取这个值。
+//     // 在这个例子中，我们将 “dark” 作为当前的值传递下去。
+//     return (
+//       <ThemeContext.Provider value="dark">
+//         <Toolbar />
+//       </ThemeContext.Provider>
+//     );
+//   }
+// }
+
+// function Button(props) {
+//   return (
+//     <div>
+//       {props.theme}
+//     </div>
+//   );
+// }
+
+// // 中间的组件再也不必指明往下传递 theme 了。
+// function Toolbar() {
+//   return (
+//     <div>
+//       <ThemedButton />
+//     </div>
+//   );
+// }
+
+// class ThemedButton extends React.Component {
+//   // 指定 contextType 读取当前的 theme context。
+//   // React 会往上找到最近的 theme Provider，然后使用它的值。
+//   // 在这个例子中，当前的 theme 值为 “dark”。
+//   static contextType = ThemeContext;
+//   render() {
+//     return <Button theme={this.context} />;
+//   }
+// }
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// )
+
+
+// 在嵌套组件中更新 Context
+// import {ThemeContext, themes} from './theme-context';
+// import ThemedButton from './themed-button';
+
+// // 一个使用 ThemedButton 的中间组件
+// function Toolbar(props) {
+//   return (
+//     <ThemedButton onClick={props.changeTheme}>
+//       Change Theme
+//     </ThemedButton>
+//   );
+// }
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       theme: themes.light
+//     };
+
+//     this.toggleTheme = () => {
+//       this.setState(state => ({
+//         theme:
+//           state.theme === themes.dark
+//             ? themes.light
+//             : themes.dark,
+//       }));
+//     };
+//   }
+
+//   render() {
+//     // 在 ThemeProvider 内部的 ThemedButton 按钮组件使用 state 中的 theme 值，
+//     // 而外部的组件使用默认的 theme 值
+//     return (
+//       <>
+//         <ThemeContext.Provider value={this.state.theme}>
+//           <Toolbar changeTheme={this.toggleTheme} />
+//         </ThemeContext.Provider>
+//         <ThemedButton />
+//       </>
+//     )
+//   }
+// }
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
