8c4559e94bf165dc58867a76f6952322cf90eaaf
13772846495 <934007690@qq.com>
2021-05-11 20:26:35 +0800

    React-错误边界

--- src/index.js
+++ src/index.js
@@ -818,49 +818,49 @@ import './index.css';
 //     };
 //   }
 
-  static getDerivedStateFromError(error) {
-    // 更新 state 使下一次渲染能够显示降级后的 UI
-    return {
-      hasError: true
-    };
-  }
+//   static getDerivedStateFromError(error) {
+//     // 更新 state 使下一次渲染能够显示降级后的 UI
+//     return {
+//       hasError: true
+//     };
+//   }
 
-  componentDidCatch(error, errorInfo) {
-    // 你同样可以将错误日志上报给服务器
-    logErrorToMyService(error, errorInfo);
-  }
+//   componentDidCatch(error, errorInfo) {
+//     // 你同样可以将错误日志上报给服务器
+//     logErrorToMyService(error, errorInfo);
+//   }
 
-  render() {
-    if(this.state.hasError) {
-      // 你可以自定义降级后的 UI 并渲染
-      return <h1>Something went wrong.</h1>;
-    }
-    return this.props.children;
-  }
-}
+//   render() {
+//     if(this.state.hasError) {
+//       // 你可以自定义降级后的 UI 并渲染
+//       return <h1>Something went wrong.</h1>;
+//     }
+//     return this.props.children;
+//   }
+// }
 
 // 如果你需要在事件处理器内部捕获错误，使用普通的 JavaScript try / catch 语句：
-class MyComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       error: null
+//     };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
 
-  handleClick() {
-    try {
-      // 执行操作，如有错误则会抛出
-    } catch(error) {
-      this.setState({ error });
-    }
-  }
+//   handleClick() {
+//     try {
+//       // 执行操作，如有错误则会抛出
+//     } catch(error) {
+//       this.setState({ error });
+//     }
+//   }
 
-  render() {
-    if(this.state.error) {
-      return <h1>Caught an error.</h1>
-    }
-    return <button onClick={this.handleClick}>Click Me</button>
-  }
-}
+//   render() {
+//     if(this.state.error) {
+//       return <h1>Caught an error.</h1>
+//     }
+//     return <button onClick={this.handleClick}>Click Me</button>
+//   }
+// }
