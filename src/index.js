a5ae2cc69fb65a944a7569fbc09124e6f7afb972
13772846495 <934007690@qq.com>
2021-05-11 20:19:35 +0800

    React-错误边界

--- src/index.js
+++ src/index.js
@@ -807,3 +807,60 @@ import './index.css';
 //   <App />,
 //   document.getElementById('root')
 // );
+
+
+// 错误边界（无法捕捉事件处理器内部的错误）
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false
+    };
+  }
+
+  static getDerivedStateFromError(error) {
+    // 更新 state 使下一次渲染能够显示降级后的 UI
+    return {
+      hasError: true
+    };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // 你同样可以将错误日志上报给服务器
+    logErrorToMyService(error, errorInfo);
+  }
+
+  render() {
+    if(this.state.hasError) {
+      // 你可以自定义降级后的 UI 并渲染
+      return <h1>Something went wrong.</h1>;
+    }
+    return this.props.children;
+  }
+}
+
+// 如果你需要在事件处理器内部捕获错误，使用普通的 JavaScript try / catch 语句：
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    try {
+      // 执行操作，如有错误则会抛出
+    } catch(error) {
+      this.setState({ error });
+    }
+  }
+
+  render() {
+    if(this.state.error) {
+      return <h1>Caught an error.</h1>
+    }
+    return <button onClick={this.handleClick}>Click Me</button>
+  }
+}
