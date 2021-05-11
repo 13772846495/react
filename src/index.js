749e367099f34369b46c7d48c2ef0f7f5395a638
13772846495 <934007690@qq.com>
2021-05-11 21:33:46 +0800

    React - Refs & DOM

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import ReactDOM, { createPortal } from 'react-dom';
 import './index.css';
 
@@ -864,3 +864,175 @@ import './index.css';
 //     return <button onClick={this.handleClick}>Click Me</button>
 //   }
 // }
+
+
+// Refs & DOM
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.myRef = React.createRef();
+//   }
+//   render() {
+//     return <div ref={this.myRef} />;
+//   }
+// }
+
+// class CustomTextInput extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     // 创建一个 ref 来存储 textInput 的 DOM 元素
+//     this.textInput = React.createRef();
+//     this.focusTextInput = this.focusTextInput.bind(this);
+//   }
+
+//   // 挂载后就模拟点击焦点
+//   componentDidMount() {
+//     this.focusTextInput();
+//   }
+
+//   focusTextInput() {
+//     // 直接使用原生 API 使 text 输入框获得焦点
+//     // 注意：我们通过 "current" 来访问 DOM 节点
+
+//     // console.log(this.textInput.current);
+//     this.textInput.current.focus();
+//   }
+
+//   render() {
+//     // 告诉 React 我们想把 <input> ref 关联到
+//     // 构造器里创建的 `textInput` 上
+//     return (
+//       <div>
+//         <input
+//           type="text"
+//           ref={this.textInput} />
+//         <input
+//           type="button"
+//           value="Focus the text input"
+//           onClick={this.focusTextInput}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// // 包装上面的 CustomTextInput 模拟它挂载之后立即被点击的操作
+// class AutoFocusTextInput extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.textInput = React.createRef();
+//   }
+
+//   componentDidMount() {
+//     this.textInput.current.focusTextInput();
+//   }
+
+//   render() {
+//     return (
+//       <CustomTextInput ref={this.textInput} />
+//     );
+//   }
+// }
+
+// ReactDOM.render(
+//   <CustomTextInput />,
+//   document.getElementById('root')
+// )
+
+//在函数组件内部使用 ref 属性，只要它指向一个 DOM 元素或 class 组件
+// function CustomTextInput(props) {
+//   // 这里必须声明 textInput，这样 ref 才可以引用它
+//   const textInput = useRef(null);
+
+//   function handleClick() {
+//     textInput.current.focus();
+//   }
+
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         ref={textInput} />
+//       <input
+//         type="button"
+//         value="Focus the text input"
+//         onClick={handleClick}
+//       />
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(
+//   <CustomTextInput />,
+//   document.getElementById('root')
+// )
+
+
+// 回调 Refs
+// class CustomTextInput extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.textInput = null;
+
+//     this.setTextInputRef = element => {
+//       this.textInput = element;
+//     };
+
+//     this.focusTextInput = () => {
+//       // 使用原生 DOM API 使 text 输入框获得焦点
+//       if(this.textInput)
+//         this.textInput.focus();
+//     }
+//   }
+
+//   componentDidMount() {
+//     // 组件挂载后，让文本框自动获得焦点
+//     this.focusTextInput();
+//   }
+
+//   render() {
+//     // 使用 `ref` 的回调函数将 text 输入框 DOM 节点的引用存储到 React
+//     // 实例上（比如 this.textInput）
+//     return (
+//       <div>
+//         <input
+//           type="text"
+//           ref={this.setTextInputRef}
+//         />
+//         <input
+//           type="button"
+//           value="Focus the text input"
+//           onClick={this.focusTextInput}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(
+//   <CustomTextInput />,
+//   document.getElementById('root')
+// )
+
+
+function CustomTextInput(props) {
+  return (
+    <div>
+      <input ref={props.inputRef} />
+    </div>
+  );
+}
+
+class Parent extends React.Component {
+  render() {
+    return (
+      <CustomTextInput inputRef={el => this.inputElement = el} />
+    );
+  }
+}
+
+ReactDOM.render(
+  <Parent />,
+  document.getElementById('root')
+)
